d89cb6f28d7d894a69b2d5833a1cab20d4bbd17a
mvilone <mahervilone20@gmail.com>
2023-05-26 17:40:48 -0400

    added api response grab for current weather data

--- WeatherApp/src/main/java/com/example/WeatherApp/database/CityInterface.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/database/CityInterface.java
@@ -1,7 +1,9 @@
 package com.example.WeatherApp.database;
 
+import java.io.IOException;
+
 public interface CityInterface {
-    public String getTempInCelcius();
+    public String getTempInCelcius() throws IOException;
     public String getTempInFahrenheit();
     public City getCity();
     public boolean equals();
