4744871814f600bc82d9809d67055c5a4c7c94f4
mvilone <mahervilone20@gmail.com>
2023-05-27 16:25:21 -0400

    setting up the classes for Hourly and Daily data for a city

--- WeatherApp/src/main/java/com/example/WeatherApp/database/City.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/database/City.java
@@ -2,6 +2,8 @@ package com.example.WeatherApp.database;
 
 public class City implements CityInterface{
     CustomHashMap<City> data_collection = new CustomHashMap<>();
+    CustomHashMap<Day> past_five_day = new CustomHashMap<>();
+    CustomHashMap<Day> future_five_day = new CustomHashMap<>();
     public String getTempInCelcius(){
         return null;
     }
