3cea382b86aefe9f64110a8f2c49cf62da80c1a1
mvilone <mahervilone20@gmail.com>
2023-05-28 01:14:59 -0400

    correcting and fully finishing HashMap

--- WeatherApp/src/main/java/com/example/WeatherApp/database/CustomHashMap.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/database/CustomHashMap.java
@@ -3,12 +3,10 @@ package com.example.WeatherApp.database;
 public class CustomHashMap<T> extends CustomLinkedList<T> {
     
     private CustomLinkedList<T>[] array_in_Hash;
-    private int size_of_array;
     @SuppressWarnings("unchecked")
     public CustomHashMap(){
         array_in_Hash = new CustomLinkedList[20];
         fillHash();
-        size_of_array = -1;
     }
     public void fillHash(){
         for(int x = 0; x < array_in_Hash.length; ++x){
@@ -20,13 +18,11 @@ public class CustomHashMap<T> extends CustomLinkedList<T> {
         if(obtain_element(element) != null){
             return false;
         }
-        size_of_array += 1;
         array_in_Hash[index_to_place].append_Node(element);
         return true;
     }
     public T obtain_element(T element){
         int index_to_place = element.hashCode() % array_in_Hash.length;
-        size_of_array += 1;
         CustomNode iterateNode = array_in_Hash[index_to_place].get_the_head();
         while(iterateNode != null){
             if(iterateNode.getValueNode().equals(element)){
@@ -34,6 +30,13 @@ public class CustomHashMap<T> extends CustomLinkedList<T> {
             }
             iterateNode = iterateNode.getNextNode();
         }
+        return null;
+    }
+    public T remove_element(T element){
+        int index_to_place = element.hashCode() % array_in_Hash.length;
+        CustomLinkedList<T> linked_list;
+        linked_list = array_in_Hash[index_to_place];
+        linked_list.remove_Node(element);
         return null;
     }
     void dispArray(){
@@ -63,6 +66,9 @@ public class CustomHashMap<T> extends CustomLinkedList<T> {
         array.append_element(19);
         array.append_element(20);
         array.append_element(20);
+        array.dispArray();
+        array.remove_element(20);
+        array.remove_element(0);
 
         array.dispArray();
         System.out.println(array.obtain_element(19)+ " element obtained");
