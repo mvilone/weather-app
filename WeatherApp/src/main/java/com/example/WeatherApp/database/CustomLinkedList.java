d73dbca526f5e5740303afb9a3ade5ab8aeb0ffa
mvilone <mahervilone20@gmail.com>
2023-05-28 01:14:59 -0400

    correcting and fully finishing HashMap

--- WeatherApp/src/main/java/com/example/WeatherApp/database/CustomLinkedList.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/database/CustomLinkedList.java
@@ -19,6 +19,9 @@ public class CustomLinkedList <T>{
         public void setValueNode(T new_val_in_node){
             this.val_in_node = new_val_in_node;
         }
+        public void setNextNode(CustomNode value_new){
+            this.next_node = value_new;
+        }
     }
     public CustomLinkedList(){
         this.the_head_node = null;
@@ -32,14 +35,77 @@ public class CustomLinkedList <T>{
     }
 
     public boolean append_Node(T element){
+        CustomNode node_element = new CustomNode(element, null);
         if(the_head_node == null){
             the_head_node = new CustomNode(element, null);
             the_tail_node = the_head_node;
             return true;
         }
-        the_head_node = new CustomNode(element, the_head_node);
+        the_tail_node.setNextNode(node_element);
+        the_tail_node = the_tail_node.next_node;
         return true;
 
+    }
+
+    public boolean remove_Node(T element){
+        if(the_head_node == null){
+            return false;
+        }
+        CustomNode iterateNode = the_head_node;
+        CustomNode prevNode = null;
+        CustomNode nextNode = null;
+        boolean element_not_found = true;
+        while((iterateNode != null) && (element_not_found == true)){
+            System.out.println("true");
+            if(iterateNode.getValueNode().equals(element)){
+                nextNode = iterateNode.getNextNode();
+                element_not_found = false;
+            }
+            else{
+                prevNode = iterateNode;
+                iterateNode = iterateNode.getNextNode();
+            }
+        }
+        System.out.println("hello");
+        if(element_not_found == true){
+            return false;
+        }
+        System.out.println("hello");
+        if(prevNode == null){
+            System.out.println("hello");
+            the_head_node = the_head_node.getNextNode();
+        }
+        else{
+            prevNode.setNextNode(iterateNode.getNextNode());
+            the_tail_node = prevNode;
+        }
+
+        return false;
+
+    }
+    public void dispArray2(){
+        CustomNode iterateNode = the_head_node;
+        System.out.print("[");
+        while(iterateNode != null){
+            System.out.print(iterateNode.getValueNode() + ", ");
+            iterateNode = iterateNode.getNextNode();
+        }
+        System.out.println("]");
+
+
+    }
+
+    public static void main(String[] args){
+        CustomLinkedList<Integer> array = new CustomLinkedList<>();
+        array.append_Node(1);
+        array.append_Node(2);
+        array.append_Node(3);
+        array.append_Node(4);
+        System.out.println(array.get_the_tail().getValueNode());
+        array.dispArray2();
+        array.remove_Node(3);
+        array.dispArray2();
+        //array.dispArray2();
     }
     
 }
