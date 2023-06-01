bfb8d3f17994a02bd76ce4b1ac85c7b0003a57e1
mvilone <mvilone20@gmail.com>
2023-06-01 16:36:11 -0400

    getCityHourlyWeather, CommonWeatherData are the major changes

--- WeatherApp/src/main/java/com/example/WeatherApp/model/City.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/model/City.java
@@ -1,29 +1,52 @@
 package com.example.WeatherApp.model;
 import com.example.WeatherApp.database.CustomHashMap;
+import com.example.WeatherApp.api.WeatherAppController;
 
 public class City implements CityInterface{
+    private final int number_of_days = 5;
+    private String city_name;
     CurrentWeather currentweather = new CurrentWeather();
     CustomHashMap<Day> past_five_days = new CustomHashMap<>();
     CustomHashMap<Day> future_five_days = new CustomHashMap<>();
 
-    public City() {
+    public City(String city_name){
+        this.city_name = city_name;
+        populate_hashes();
+        try{
+            currentweather = WeatherAppController.getCurrentWeatherCitySearch(city_name);
+            System.out.println(currentweather.getCurrent().getTemp_c());
+        }
+        catch (Exception e){
+            System.err.println("Error city not found");
+        }
     }
 
-    public String getTempInCelcius() {
-
-        return null;
+    public void populate_hashes(){
+        Day d1 = null;
+        Day d2 = null;
+        for(int x = 0; x < number_of_days; ++x){
+            d1 = new Day();
+            d2 = new Day();
+            d1.set_days_city(this);
+            d2.set_days_city(this);
+            past_five_days.append_element(d1);
+            future_five_days.append_element(d2);
+        }
     }
-    public String getTempInFahrenheit(){
-        return null;
-    }
-    public City getCity(){
-        return null;
+    public String get_city_name(){
+        return city_name;
     }
     public boolean equals(){
         return false;
     }
+    public String toString(){
+        return city_name;
+    }
     public static void main(String[] args){
-        System.out.println("hello");
+        City c = new City("chantilly");
+        c.past_five_days.dispArray();
+        c.future_five_days.dispArray();
+
     }
     
 }
--- WeatherApp/src/main/java/com/example/WeatherApp/model/Day.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/model/Day.java
@@ -1,6 +1,27 @@
 package com.example.WeatherApp.model;
+import com.example.WeatherApp.database.CustomHashMap;
 
-public class Day extends City{
+public class Day{
+    private City day_city;
+    public CustomHashMap<Hour> twenty_4_hrs = new CustomHashMap<Hour>();
+    private Forecast forecast;
+    private Location location;
+    public Day(){
+        
+    }
+    public void set_days_city(City city){
+        this.day_city = city;
+    }
+    public String toString(){
+
+        return day_city.toString();
+    }
+    public Forecast getForecast(){
+        return forecast;
+    }
+    public Location getLocation(){
+        return location;
+    }
     
     
 }
