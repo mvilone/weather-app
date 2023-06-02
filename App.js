c4609a24865697c8c1b423bb1c8c75e64c6c6d38
mvilone <mahervilone20@gmail.com>
2023-06-02 18:47:50 -0400

    Added multiple pages, but display is wrong. Also place to enter a user email & password

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopButtons from './componenets/TopButtons';
-import Inputs from './componenets/Inputs';
-import TimeAndLocation from './componenets/TimeAndLocation';
-import Details from './componenets/Details';
-import Forecast from './componenets/Forecast';
-
+import Login from './Login';
+import Register from './Register';
+import Content from './Content';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-200 to-blue-700 h-fit shadow-xl
-    shadow-gray-400">
-      <TopButtons />
-      <Inputs />
-
-      <TimeAndLocation />
-      <Details />
-
-      <Forecast title = "Hourly Forecast" />
-      <Forecast title = "Daily Forecast" />
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register"element = {<Register />} />
+        <Route path="/content"element = {<Content />} />
+      </Routes>
     </div>
   );
   
