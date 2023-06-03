4e5e5f4883887d28bee79741374c3471a0b62212
mvilone <mvilone20@gmail.com>
2023-06-03 19:01:21 -0400

    Implementing previous days data and the hourly

--- WeatherApp/src/main/java/com/example/WeatherApp/model/CurrentWeather.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/model/CurrentWeather.java
@@ -6,7 +6,7 @@ import com.example.WeatherApp.model.Location;
 
 public class CurrentWeather {
 
-    private Location locatio;
+    private Location location;
     private CurrentWeatherInfo current;
 
     public void setCurrent(CurrentWeatherInfo current) {
@@ -14,11 +14,11 @@ public class CurrentWeather {
     }
 
     public void setLocation(Location location) {
-        this.locatio = location;
+        this.location = location;
     }
 
     public Location getLocation() {
-        return locatio;
+        return location;
     }
 
 
