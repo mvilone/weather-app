2ef116fde9ed5b54b8ae53db8dc2f8e8ceb25835
mvilone <mahervilone20@gmail.com>
2023-06-07 00:27:39 -0400

    added a class TestHashMap to test HashMap

---
+++ WeatherApp/src/test/java/com/example/WeatherApp/TestHashMap.java
@@ -0,0 +1,9 @@
+package com.example.WeatherApp;
+import com.example.WeatherApp.database.CustomHashMap;
+public class TestHashMap {
+    public static void main(String []args){
+        CustomHashMap<Integer> a = new CustomHashMap<>();
+
+    }
+    
+}
