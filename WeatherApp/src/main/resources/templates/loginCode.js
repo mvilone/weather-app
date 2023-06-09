c309c7db590d4a617698923f273815a75d64f937
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2023-06-09 16:42:54 -0400

    authentication completed

--- WeatherApp/src/main/resources/templates/loginCode.js
+++ WeatherApp/src/main/resources/templates/loginCode.js
@@ -1,53 +1,86 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  const firebaseConfig = {
-    apiKey: "<REMOVED>",
-    authDomain: "weatherapp-bb11c.firebaseapp.com",
-    databaseURL: "https://weatherapp-bb11c-default-rtdb.firebaseio.com",
-    projectId: "weatherapp-bb11c",
-    storageBucket: "weatherapp-bb11c.appspot.com",
-    messagingSenderId: "770085319311",
-    appId: "1:770085319311:web:e64c780a6619418f21b6fa",
-    measurementId: "G-S452XRH8M6"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "<REMOVED>",
+  authDomain: "weatherapp-bb11c.firebaseapp.com",
+  databaseURL: "https://weatherapp-bb11c-default-rtdb.firebaseio.com",
+  projectId: "weatherapp-bb11c",
+  storageBucket: "weatherapp-bb11c.appspot.com",
+  messagingSenderId: "770085319311",
+  appId: "1:770085319311:web:e64c780a6619418f21b6fa",
+  measurementId: "G-S452XRH8M6"
+};
 
-  //initialize firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  //reference database
-  var loginDB = firebase.database().ref("CS321-firebase");
+const auth = getAuth();
 
-  document.getElementById('login').addEventListener('submit',submitForm);
+ //----- New Registration code start
+  document.getElementById("register").addEventListener("click", function() {
+      var email =  document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      //For new registration
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        alert("Registration successfully!!");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorMessage);
+        alert(error);
+      });
+  });
+  //----- End
 
-  function submitForm(e){
-    e.preventDefault();
+   //----- Login code start
+  document.getElementById("login").addEventListener("click", function() {
+      var email =  document.getElementById("login_email").value;
+      var password = document.getElementById("login_password").value;
 
-    var emailid = getElementVal('emailid');
-    var password = getElementVal('password');
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        alert(user.email+" Login successfully!!!");
+        document.getElementById('logout').style.display = 'block';
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        alert(errorMessage);
+      });
+  });
+  //----- End
 
-    saveLogin(emailid, password)
+  //----- Logout code start
+  document.getElementById("logout").addEventListener("click", function() {
+      signOut(auth).then(() => {
+          // Sign-out successful.
+          console.log('Sign-out successful.');
+          alert('Sign-out successful.');
+          document.getElementById('logout').style.display = 'none';
+        }).catch((error) => {
+          // An error happened.
+          console.log('An error happened.');
+        });
+  });
+  //----- End
 
-    //enable alert
-    document.querySelector(".alert").style.display = "block";
-
-    //remove alert
-    setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-    }, 3000);
-
-    //reset the form
-    document.getElementById('login').reset();
-
-  }
-
-  const saveLogin = (emailid, password) => {
-    var newLogin = loginDB.push();
-
-    newLogin.set({
-        emailid : emailid,
-        password : password,
-    });
-  };
-
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  }
