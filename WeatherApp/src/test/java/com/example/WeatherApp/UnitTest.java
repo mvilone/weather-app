64fe89f3eda1ae0f76c23c9e8492a0d099f1c0dd
mvilone <mahervilone20@gmail.com>
2023-06-11 17:47:38 -0400

    We as a group made edge case tests for the api testing

--- WeatherApp/src/test/java/com/example/WeatherApp/UnitTest.java
+++ WeatherApp/src/test/java/com/example/WeatherApp/UnitTest.java
@@ -6,8 +6,16 @@ import com.example.WeatherApp.api.WeatherAppController;
 import com.example.WeatherApp.model.*;
 import java.io.IOException;
 @SpringBootTest
+/**
+ * This class tests that the api extraction and behavior.
+ */
 public class UnitTest{
     @Test
+    /**
+     * This method tests that the attribute Temperature in celcius 
+     * was obtained successfully from the api into an object.
+     * @throws IOException
+     */
     void Test1() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String city: input){
@@ -16,6 +24,11 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests that the attribute Temperature in fahrenheit 
+     * was obtained successfully from the api into an object.
+     * @throws IOException
+     */
     void Test2() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String city: input){
@@ -24,6 +37,12 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests that the precipitation in inches attribute
+     * was successfully obtained from the api into an object.
+     * Only does it for cities that use inches.
+     * @throws IOException
+     */
     void Test3() throws IOException{
         String input[] = {"Fairfax", "New York"};
         for(String city: input){
@@ -32,6 +51,12 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests that the precipitation in mm attribute
+     * was successfully obtained from the api into an object.
+     * Only does it for cities that use mm.
+     * @throws IOException
+     */
     void Test4() throws IOException{
         String input[] = {"Paris", "Beijing", "Sydney"};
         for(String city: input){
@@ -40,6 +65,11 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests that the attribute local current time 
+     * was obtained successfully from the api into an object.
+     * @throws IOException
+     */
     void Test5() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String city: input){
@@ -48,6 +78,12 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests city name for CurrentWeather was succesfully obtained from
+     * the api and maps it into an object.
+     * Also makes sure its in the api's format, "Fairfax" "New York".
+     * @throws IOException
+     */
     void Test6() throws IOException{
         String input[] = {"faIrfax", "New York", "Paris", "Beijing", "Sydney"};
         String expectation[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
@@ -60,14 +96,28 @@ public class UnitTest{
     }
     
     @Test
+    /**
+     * This method tests city name for City was succesfully obtained from
+     * the api and maps it into an object.
+     * Also makes sure its in the api's format, "Fairfax" "New York".
+     * @throws IOException
+     */
     void Test7() throws IOException{
-        String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
+        String input[] = {"FaIrfax", "New York", "Paris", "Beijing", "Sydney"};
+        String expectation[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
+        int i = 0;
         for(String c: input){
             City city = WeatherAppController.getPreviousDaysForCity(c, 5);
-            assertEquals(c, city.get_city_name());
+            assertEquals(expectation[i], city.get_city_name());
+            i++;
         }
     }
     @Test
+    /**
+     * This method tests that the previous five day's weather data were successfully
+     * obtained from the api successfully by seeing that one day exists in the object City.
+     * @throws IOException
+     */
     void Test8() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String c: input){
@@ -76,6 +126,11 @@ public class UnitTest{
         }
     }
     @Test
+    /** 
+     * This method tests that from the previous five days
+     * a certain day's 24 hrs weather data was obtained from the api
+     * successfully.
+     */
     void Test9() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String c: input){
@@ -84,6 +139,11 @@ public class UnitTest{
         }
     }
     @Test
+    /**
+     * This method tests with one of days extracted in the City object
+     * that the attribute max temperature in celcius was successfully obtained from the api.
+     * @throws IOException
+     */
     void Test10() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String c: input){
@@ -91,63 +151,63 @@ public class UnitTest{
             assertTrue(city.getPastDays().obtain_element(1).getMaxtemp_c() >= -273);
         }
     }
-    void Test17() throws IOException{
+    @Test
+    /**
+     * This method tests with one of days extracted and a certain
+     * hour extracted from it in the City object
+     * that the attribute local time was successfully obtained from the api.
+     * @throws IOException
+     */
+    void Test11() throws IOException{
         String input[] = {"Fairfax", "New York", "Paris", "Beijing", "Sydney"};
         for(String c: input){
             City city = WeatherAppController.getPreviousDaysForCity(c, 5);
             assertNotNull(city.getPastDays().obtain_element(1).getHoursMap().obtain_element(15).getTime());
         }
     }
-    
-    /*@Test
-    void Test7() throws IOException{
-        int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertNotNull(weather.getCurrent().getTemp_c());
-        }
-    }
+    //edge cases for the api testing
     @Test
-    void Test8() throws IOException{
-        int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertNotNull(weather.getCurrent().getTemp_f());
-        }
-    }
-    @Test
-    void Test9() throws IOException{
-        int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertNotNull(weather.getCurrent().getPrecip_in());
-        }
-    }
-    @Test
-    void Test10() throws IOException{
-        int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertNotNull(weather.getCurrent().getPrecip_mm());
-        }
-    }
-    @Test
-    void Test11() throws IOException{
-        int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertNotNull(weather.getLocation().getLocaltime());
-        }
-    }
-    @Test
+    /**
+     * This tests that what gets placed in the api for city_name
+     * contains character a-z or A-Z and space character.
+     * Basically doesn't allow any other character.
+     * @throws IOException
+     */
     void Test12() throws IOException{
-       int input[] = {22033, 10008, 70123, 065001, 2000};
-        for(int zipcode: input){
-            CurrentWeather weather = WeatherAppController.getCurrentWeatherZipcodeSearch(zipcode);
-            assertEquals(zipcode, weather.getLocation().getName());
+        String city_name = "#";
+        boolean exceptionThrown = false;
+        try{
+            WeatherAppController.getCurrentWeatherCitySearch(city_name);
         }
-    }*/
+        catch(IOException e){
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+    @Test
+    /**
+     * This method checks that the input for city is an actual 
+     * city and that it actually exisits in the world or api.
+     * @throws IOException
+     */
+    void Test13() throws IOException{
+        String city_name = "jsiddusidjsn";
+        boolean exceptionThrown = false;
+        try{
+            WeatherAppController.getCurrentWeatherCitySearch(city_name);
+        }
+        catch(Exception e){
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
 
+    }
+
+    
+
+    
+    
+    
 
 
     
