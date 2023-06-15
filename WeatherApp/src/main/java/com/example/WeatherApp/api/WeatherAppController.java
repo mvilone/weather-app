8ea4df1130c364488a23c1f2ac35400b51e23af8
mvilone <mahervilone20@gmail.com>
2023-06-15 07:14:40 -0400

    Api extraction 100 % complete, I populate city object with everything needed. Takes 1 to 2 seconds.

--- WeatherApp/src/main/java/com/example/WeatherApp/api/WeatherAppController.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/api/WeatherAppController.java
@@ -23,6 +23,7 @@ public class WeatherAppController {
 
     private static final String  APIKEY = "<REMOVED>";
     private static final int NUMBER_OF_SEC_HR = 3600;
+    private static City cityObject = null;
 
     @RequestMapping("/home")
     public static CurrentWeather getCurrentWeatherIPGrab() throws IOException {
@@ -38,7 +39,6 @@ public class WeatherAppController {
     }
 
     public static CurrentWeather getCurrentWeatherCitySearch(String cityName) throws IOException {
-        System.out.println(containsNonCharacter(cityName) + " " + cityName);
         if(containsNonCharacter(cityName)){
             throw new IOException("Input must be a-z or A-Z");
         }
@@ -64,78 +64,175 @@ public class WeatherAppController {
 
         return currentWeather;
     }
+    public static ForecastedWeather getForecastWeatherIPGrab() throws IOException {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key="+APIKEY+"&days=5&q=auto:ip";
+
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.getForEntity(url, String.class);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        final ForecastedWeather forecast = objectMapper.readValue(response.getBody(), ForecastedWeather.class);
+
+        return forecast;
+    }
+
+    public static ForecastedWeather getForecastWeatherCitySearch(String cityName, String file, int unixdate) throws IOException {
+        if(containsNonCharacter(cityName)){
+            throw new IOException("Input must be a-z or A-Z");
+        }
+        String url = "http://api.weatherapi.com/v1/"+file+"?key="+APIKEY+"&q="+cityName+"&unixdt="+unixdate;
+
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.getForEntity(url, String.class);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        final ForecastedWeather forecast = objectMapper.readValue(response.getBody(), ForecastedWeather.class);
+
+        return forecast;
+    }
+
+    public static ForecastedWeather getForecastWeatherZipcodeSearch(int zipcode) throws IOException {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key="+APIKEY+"&days=5&q="+String.valueOf(zipcode);
+
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.getForEntity(url, String.class);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        final ForecastedWeather forecast = objectMapper.readValue(response.getBody(), ForecastedWeather.class);
+
+        return forecast;
+    }
+    
     public static City getPreviousDaysForCity(String cityName, int xDay) throws IOException{
         if(containsNonCharacter(cityName)){
             throw new IOException("Input must be a-z or A-Z");
         }
         CurrentWeather current = getCurrentWeatherCitySearch(cityName);
         int unix_date_time = current.getLocation().getLocaltime_epoch();
-        System.out.println(unix_date_time);
         int number_of_secondInXDay = (NUMBER_OF_SEC_HR * 24) * xDay;
         unix_date_time -= (number_of_secondInXDay);
-        System.out.println(unix_date_time);
-        final ObjectMapper objectMapper = new ObjectMapper();
-        RestTemplate rt = new RestTemplate();
-        City instanceCity = null;
-        City mergedCity = new City(current.getLocation().getName());
+        ForecastedWeather instanceForecasted = null;
         for(int x = 0; x < xDay; ++x){
-            String url = "http://api.weatherapi.com/v1/history.json?key="+APIKEY+"&q="+cityName+"&unixdt="+unix_date_time;
-            ResponseEntity<String> response = rt.getForEntity(url, String.class);
-            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            instanceCity = objectMapper.readValue(response.getBody(), City.class);
-            instanceCity = objectMapper.readValue(response.getBody(), City.class);
-            Day current_day = instanceCity.getForecast().getForecastday().get(0).getDay();
-            current_day.setForecast(instanceCity.getForecast().getForecastday().get(0));
+            instanceForecasted = getForecastWeatherCitySearch(cityName, "history.json", unix_date_time);
+            Day current_day = instanceForecasted.getForecast().getForecastday().get(0).getDay();
+            current_day.setForecast(instanceForecasted.getForecast().getForecastday().get(0));
             current_day.set_Day_Number(x + 1);
-            mergedCity.addToPastXDays(current_day);
+            cityObject.addToPastXDays(current_day);
             unix_date_time += (NUMBER_OF_SEC_HR * 24);
         }
         for(int x = 0; x < xDay; ++x){
             for(int y = 0; y < 24; ++y){
-                Hour h = mergedCity.getPastDays().obtain_element(x + 1).getForecast().getHour().get(y);
+                Hour h = cityObject.getPastDays().obtain_element(x + 1).getForecast().getHour().get(y);
                 h.set_Hour_Number(y);
-                mergedCity.getPastDays().obtain_element(x + 1).addToHours(h);
+                cityObject.getPastDays().obtain_element(x + 1).addToHours(h);
             }
         }
-        return mergedCity;
+        return cityObject;
+    }
+
+    public static City getNextDaysForCity(String cityName, int xDay) throws IOException{
+        if(containsNonCharacter(cityName)){
+            throw new IOException("Input must be a-z or A-Z");
+        }
+        CurrentWeather current = getCurrentWeatherCitySearch(cityName);
+        int unix_date_time = current.getLocation().getLocaltime_epoch();
+        unix_date_time += NUMBER_OF_SEC_HR * 24;
+        ForecastedWeather instanceForecasted = null;
+        for(int x = 0; x < xDay; ++x){
+            instanceForecasted = getForecastWeatherCitySearch(cityName, "forecast.json", unix_date_time);
+            Day current_day = instanceForecasted.getForecast().getForecastday().get(0).getDay();
+            current_day.setForecast(instanceForecasted.getForecast().getForecastday().get(0));
+            current_day.set_Day_Number(x + 1);
+            cityObject.addToFutureXDays(current_day);
+            unix_date_time += (NUMBER_OF_SEC_HR * 24);
+        }
+        for(int x = 0; x < xDay; ++x){
+            for(int y = 0; y < 24; ++y){
+                Hour h = cityObject.getFutureDays().obtain_element(x + 1).getForecast().getHour().get(y);
+                h.set_Hour_Number(y);
+                cityObject.getFutureDays().obtain_element(x + 1).addToHours(h);
+            }
+        }
+        return cityObject;
     } 
+
+    public static CurrentWeather getHourlyForCurrent(CurrentWeather currentweather)throws IOException{
+        String cityName = currentweather.getLocation().getName();
+        int currentDateInUnix = currentweather.getLocation().getLocaltime_epoch();
+        ForecastedWeather forecastedweather = getForecastWeatherCitySearch(cityName, "forecast.json", currentDateInUnix);
+        Day currentDay = forecastedweather.getForecast().getForecastday().get(0).getDay();
+        currentDay.setForecast(forecastedweather.getForecast().getForecastday().get(0));
+        for(int x = 0; x < 24; ++x){
+            Hour h = currentDay.getForecast().getHour().get(x);
+            h.set_Hour_Number(x);
+            currentDay.addToHours(h);
+        }
+        currentweather.setCurrentDay(currentDay);
+        return currentweather;
+
+    }
     public static boolean containsNonCharacter(String input){
         input = input.replaceAll(" ", "");
         for(int x = 0; x < input.length(); ++x){
             char c = input.charAt(x);
             if(!(((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))){
-                System.out.println("true");
                 return true;
             }
         }
         return false;
 
     }
-    /*public static Day getCityHourlyWeatherForDay(String cityName, String date) throws IOException{
-        final ObjectMapper objectMapper = new ObjectMapper();
-        String url = "http://api.weatherapi.com/v1/history.json?key="+APIKEY+"&q="+cityName+"&dt="+date;
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.getForEntity(url, String.class);
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        Day instanceDay  = objectMapper.readValue(response.getBody(), Day.class);
-        ArrayList<Hour> twenty_4_hr = instanceDay.getForecast().getForecastday().get(0).getHour();
-        int hour_number = 1;
-        for(Hour x: twenty_4_hr){
-            x.set_Hour_Number(hour_number);
-            instanceDay.twenty_4_hrs.append_element(x);
-            hour_number += 1;
+    public static void setCityObject(String cityName){
+        cityObject = new City(cityName);
+    }
+
+    public static CurrentWeather initializeCityObject(char flag, String cityName, int zipcode) throws IOException{
+        CurrentWeather currentweather = new CurrentWeather();
+        switch(flag) {
+            case 'i':
+                currentweather = getCurrentWeatherIPGrab();
+                break;
+            case 'z':
+                currentweather = getCurrentWeatherZipcodeSearch(zipcode);
+                break;
+            case 'c':
+                currentweather = getCurrentWeatherCitySearch(cityName);
+                break;
+            default:
+                throw new IOException("flag has to be i, z, c");
         }
-        return instanceDay;
-        
-    }*/
+        setCityObject(currentweather.getLocation().getName());
+        currentweather = getHourlyForCurrent(currentweather);
+        return currentweather;
+
+    }
+    public static City populateCity(char flag, String cityName, int zipcode) throws IOException{
+        cityObject = null;
+        String flagString = String.valueOf(flag).toLowerCase();
+        flag = flagString.charAt(0);
+        CurrentWeather currentweather = initializeCityObject(flag, cityName, zipcode);
+        cityObject.setCurrentWeather(currentweather);
+        getPreviousDaysForCity(currentweather.getLocation().getName(), 5);
+        getNextDaysForCity(currentweather.getLocation().getName(), 5);
+
+        return cityObject;
+
+    }
     public static void main(String [] args) throws IOException{
-        City object = getPreviousDaysForCity("addis", 5);
-        System.out.println(object.get_city_name());
-        System.out.println(object.getPastDays().obtain_element(1).getForecast().getDay());
-        System.out.println(object.getPastDays().obtain_element(1).getForecast().getDay().getHoursMap().obtain_element(23));
-        System.out.println(object.getPastDays());
-        System.out.println(object.getPastDays().obtain_element(1).getForecast().getDay().getHoursMap());
-        //getCityHourlyWeatherForDay("chantilly", "2023-06-01");
+        City test = populateCity('z', null, 77494);
+        System.out.println(test.getCurrentWeather().getCurrent().getFeelslike_f());
+        System.out.println(test.getCurrentWeather().getCurrentDay());
+        System.out.println(test.getCurrentWeather().getCurrentDay().getHoursMap());
+        System.out.println(test.get_city_name());
+        System.out.println(test.getPastDays().obtain_element(1).getForecast().getDay());
+        System.out.println(test.getPastDays().obtain_element(1).getForecast().getDay().getHoursMap().obtain_element(23));
+        System.out.println(test.getPastDays());
+        System.out.println(test.getPastDays().obtain_element(1).getForecast().getDay().getHoursMap());
+        System.out.println(test.get_city_name());
+        System.out.println(test.getFutureDays().obtain_element(1).getForecast().getDay());
+        System.out.println(test.getFutureDays().obtain_element(1).getForecast().getDay().getHoursMap().obtain_element(23));
+        System.out.println(test.getFutureDays());
+        System.out.println(test.getFutureDays().obtain_element(1).getForecast().getDay().getHoursMap());
 
     }
 
