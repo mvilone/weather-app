970a890b6b86b770daaa32d13abb8470e9832b9c
mvilone <mvilone20@gmail.com>
2023-06-15 07:18:52 -0400

    removed getForecastWeather for Ip and zipcode removed
    I used getCurrentWeather for ip, zipcode, city, for getForecastWeather for city, current weather gets the city with ip, zipcode, or city input.

--- WeatherApp/src/main/java/com/example/WeatherApp/api/WeatherAppController.java
+++ WeatherApp/src/main/java/com/example/WeatherApp/api/WeatherAppController.java
@@ -64,17 +64,6 @@ public class WeatherAppController {
 
         return currentWeather;
     }
-    public static ForecastedWeather getForecastWeatherIPGrab() throws IOException {
-        String url = "http://api.weatherapi.com/v1/forecast.json?key="+APIKEY+"&days=5&q=auto:ip";
-
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.getForEntity(url, String.class);
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        final ForecastedWeather forecast = objectMapper.readValue(response.getBody(), ForecastedWeather.class);
-
-        return forecast;
-    }
 
     public static ForecastedWeather getForecastWeatherCitySearch(String cityName, String file, int unixdate) throws IOException {
         if(containsNonCharacter(cityName)){
@@ -91,17 +80,6 @@ public class WeatherAppController {
         return forecast;
     }
 
-    public static ForecastedWeather getForecastWeatherZipcodeSearch(int zipcode) throws IOException {
-        String url = "http://api.weatherapi.com/v1/forecast.json?key="+APIKEY+"&days=5&q="+String.valueOf(zipcode);
-
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.getForEntity(url, String.class);
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        final ForecastedWeather forecast = objectMapper.readValue(response.getBody(), ForecastedWeather.class);
-
-        return forecast;
-    }
     
     public static City getPreviousDaysForCity(String cityName, int xDay) throws IOException{
         if(containsNonCharacter(cityName)){
