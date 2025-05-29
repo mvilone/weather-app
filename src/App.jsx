c2721b974284496af85b151cdb94bda41826dce3
mvilone <mahervilone20@gmail.com>
2025-05-29 03:53:36 -0400

    Update App and TopButtons components; add Inputs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { FaReact } from "react-icons/fa";
+import Inputs from "./components/Inputs"
+import TopButtons from "./components/TopButtons"
 const App = () => {
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br
     shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
+      <Inputs />
     </div>
   )
 };
