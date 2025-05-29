db4f684ed76c32058cb3557be73e7602921d79a6
mvilone <mvilone20@gmail.com>
2025-05-29 13:27:48 -0400

    Complete initial UI implementation with App, Forecast, TempAndDetails, and TimeAndLocation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import Forecast from "./components/Forecast"
 import Inputs from "./components/Inputs"
+import TimeandLocation from "./components/TimeandLocation";
+import TempAndDetails from "./components/TempAndDetails";
 import TopButtons from "./components/TopButtons"
 const App = () => {
   return (
@@ -6,6 +9,10 @@ const App = () => {
     shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
       <Inputs />
+      <TimeandLocation />
+      <TempAndDetails />
+      <Forecast />
+      <Forecast />
     </div>
   )
 };
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TimeandLocation = () => {
+  return (
+    <div>
+        <div className="flex iterms-center justify-center my-6">
+            <p className="text-xl font-extralight">
+                Thursday, 29 May 2024 | Local time: 12:08 PM 
+            </p>
+        </div>
+        <div className="flex items-center justify-center my-3">
+            <p className="text-3xl font-medium">Berlin, DE</p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeandLocation
