7c7b1a76846ee2236954e9838f5a96e0173ab3b9
mvilone <mvilone20@gmail.com>
2025-05-28 21:53:28 -0400

    Copied frontend files from my other repository into this project

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Login from './Login';
+import Register from './Register';
+import Content from './Content';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register"element = {<Register />} />
+        <Route path="/content"element = {<Content />} />
+      </Routes>
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Register () {
+    return (
+        <div>
+            <h1> Welcome to The Weather App </h1>
+        </div>
+    )
+}
+
+export default Register
