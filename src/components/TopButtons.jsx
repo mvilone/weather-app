9c0a30c31f42124d061e57008b10e7638b5d1f94
mvilone <mahervilone20@gmail.com>
2025-05-29 02:38:06 -0400

    First contruction of UI

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const TopButtons = () => {
+  const cities = [
+    {
+      id: 1, 
+      name: 'London'
+    },
+    {
+      id: 2, 
+      name: 'Sydney'
+    },
+    {
+      id: 3, 
+      name: 'Tokyo'
+    },
+    {
+      id: 4, 
+      name: 'Paris'
+    },
+    {
+      id: 5, 
+      name: 'Toronto'
+    }
+  ];
+  return (
+    <div className="flex iterms-center justify-around my-6">
+      {cities.map(city => {
+          <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">{city.name}</button>
+        })}
+    </div>
+  )
+}
+
+export default TopButtons
