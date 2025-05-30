6c7a03922ca6a0534a198ed34cbe1d3a605b6c1e
mvilone <mahervilone20@gmail.com>
2025-05-30 15:25:09 -0400

    Integrate backend fetch from React input and add CORS config to Spring Boot

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import {BiSearch, BiCurrentLocation} from "react-icons/bi";
 
 const Inputs = () => {
+  const [city, setCity] = useState("");
+  const handleSearchClick = async () => {
+    if(!city){
+      return;
+    }
+    try{
+      const response = await fetch(`http://localhost:8080/city/getCity?name=${encodeURIComponent(city)}`);
+      if(!response.ok){
+        throw new Error("Failed to fetch weather data");
+      }
+      const data = await response.json();
+      console.log("Received weather data:", data);
+    }
+    catch (err){
+      console.error("Error fetching data:", err.message);
+    }
+    console.log("Search clicked for city:", city);
+  };
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-        <input type="text" placeholder="search by city..." 
+        <input type="text" placeholder="search by city..." value={city} onChange={(e) => setCity(e.target.value)}
         className="bg-white text-black p-2 rounded-md w-full max-w-xs shadow-md focus:outline-none 
         text-xl font-light capitalize placeholder:lowercase" />
-        <BiSearch size = {30} className="cursor-pointer transition ease-out hover:scale-125"/>
+        <BiSearch size = {30} className="cursor-pointer transition ease-out hover:scale-125" onClick={handleSearchClick}/>
         <BiCurrentLocation size = {30} className="cursor-pointer transition ease-out hover:scale-125"/>
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
