5da89f4a6cb614cbc08ac443292fbc04edd93d69
mvilone <mahervilone20@gmail.com>
2025-06-02 16:02:31 -0400

    Implement current location button functionality and fix fetch handling for empty city input

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -28,6 +28,24 @@ const Inputs = ({setWeatherData}) => {
       console.error("Error fetching data:", err.message);
     }
   };
+
+  const handleSearchClick2 = async () => {
+    console.log("city: ", city);
+    try{
+      let response;
+      response = await fetch("http://localhost:8080/city/getCity");
+      console.log("1");
+      if(!response.ok){
+        throw new Error("Failed to fetch weather data");
+      }
+      const data = await response.json();
+      console.log("Data:", JSON.stringify(data, null, 2));
+      setWeatherData(data);
+    }
+    catch (err){
+      console.error("Error fetching data:", err.message);
+    }
+  };
   
   useEffect(() => {
       if (!city) {
@@ -42,7 +60,7 @@ const Inputs = ({setWeatherData}) => {
         className="bg-white text-black p-2 rounded-md w-full max-w-xs shadow-md focus:outline-none 
         text-xl font-light capitalize placeholder:lowercase" />
         <BiSearch size = {30} className="cursor-pointer transition ease-out hover:scale-125" onClick={handleSearchClick}/>
-        <BiCurrentLocation size = {30} className="cursor-pointer transition ease-out hover:scale-125"/>
+        <BiCurrentLocation size = {30} className="cursor-pointer transition ease-out hover:scale-125" onClick={handleSearchClick2}/>
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
       <button className="text-2xl font-medium transition ease-out hover:scale-125">°C</button>
